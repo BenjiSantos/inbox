15e2d64ec8d6b8c981595bf4ca8898ddba202ca4
BENJI SANTOS QUISPE <benji.santos@qaliwarma.gob.pe>
2018-07-26 16:15:03 -0500

    The test with open accounts of provider of ganache finish here, for next time implement our contract in the main network with real account

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,31 +1,39 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
-//const Web3 = require('web3');
-//const web3 = new Web3(ganache.provider());
+const Web3 = require('web3');
 
-class Car {
-    park() {
-        return 'stopped';
-    }
+// UPDATE THESE TWO LINES RIGHT HERE!!!!! <-----------------
+const provider = ganache.provider();
+const web3 = new Web3(provider);
 
-    drive() {
-        return 'vroom';
-    }
-}
+const { interface, bytecode } = require('../compile');
 
-let car;
+let accounts;
+let inbox;
 
-beforeEach(() => {
-    car = new Car();
+beforeEach(async () => {
+  // Get a list of all accounts
+  accounts = await web3.eth.getAccounts();
+  // Use one of those accounts to deploy the contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .send({ from: accounts[0], gas: '1000000' });
+
+  // ADD THIS ONE LINE RIGHT HERE!!!!! <---------------------
+  inbox.setProvider(provider);
 });
 
-describe('Car', () => {
-    it('car park',() => {
-        assert.equal(car.park(), 'stopped');
-    });
-
-    it('car drive',() => {
-        assert.equal(car.drive(), 'vroom');
-    });
+describe('Inbox', () => {
+  it('deploys a contract', () => {
+    assert.ok(inbox.options.address);
+  });
+  it('has a default message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'Hi there!');
+  });
+  it('Can change message', async() => {
+    await inbox.methods.setMessage('Bye guys').send({ from: accounts[0] });
+    const messagechange = await inbox.methods.message().call();
+    assert.equal(messagechange,'Bye guys');
+  })
 });
-
